39da37a92b6fa019d809118c2d0c5900f351e549
Ariel Kriheli <arieldk6@gmail.com>
2024-07-17 07:20:30 +0300

    done with linked, now hash

---
+++ hash_table_1.py
@@ -0,0 +1,4 @@
+# for each hash map theres a different hash function, for this case our hash 
+# function is using with ASCII table, the key - turned into sum of ASCII
+# chars for each letter, total size of arr and the value.
+
